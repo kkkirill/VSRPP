7b17aac238f1a7f9d97b1230d927b8c2cbe0cb9d
Kirill <37510854+kkkirill@users.noreply.github.com>
2019-12-09 16:43:27 +0300

    Update DrawForm.cs

--- ipr1/MiniDraw/DrawForm.cs
+++ ipr1/MiniDraw/DrawForm.cs
@@ -13,98 +13,108 @@ namespace MiniDraw
         private Pen pen;
         private SolidBrush brush;
 
-        private bool isFilled = false;
-        private bool isPainting = false;
+        private bool isFilled = false; // тановится true как только включаем галочку использовать заливку
+        private bool isPainting = false; // становится true как только мы рисуем по канвасу
 
-        private List<MyRectangle> figures;
+        private List<MyRectangle> figures; // здесь хранятся все фигуры, которые мы рисуем
 
-        Graphics graphics;
+        Graphics graphics;  // это объект графики из канваса на котором мы рисуем
 
         public DrawForm()
         {
             InitializeComponent();
 
-            graphics = canvas.CreateGraphics();
-            figures = new List<MyRectangle>();
-            pen = new Pen(Color.Black, 2);
-            brush = new SolidBrush(Color.Red);
+            graphics = canvas.CreateGraphics(); //
+            figures = new List<MyRectangle>();  //
+            pen = new Pen(Color.Black, 2);      //
+            brush = new SolidBrush(Color.Red);  //
+                                                // создали pen для рисования незакрашенных прямоугольников и 
+                                                // brush для закрашенных
         }
 
-        private void RedrawContent(bool isClearRequired = false)
+        private void RedrawContent(bool isClearRequired = false) // перерисовка фигур
         {
-            if (isClearRequired) 
+            if (isClearRequired) //если нужна очистка, то здесь будет true
                 graphics.Clear(canvas.BackColor);
 
-            var rectangles = figures.Where(fig => fig.IsDrawn)
-                                    .Select(fig => fig.Rectangle)
-                                    .ToArray();
+            var rectangles = figures.Where(fig => fig.IsDrawn)      // берём все фигуры у который флаг isDrawn == true, т.е. фигуры которые мы хотим отрисовать
+                                    .Select(fig => fig.Rectangle)   // из полученных объектов MyRectangle для каждого объекта достаём поле Rectangle, 
+                                                                    // которое можно отрисовывать на канвасе (Rectangle это встроенный класс уже, 
+                                                                    // а MyRectangle прописанный мной класс с дополнительным полем isDrawn для определения рисовать фигуру или нет) 
+                                    .ToArray();                     // преобразовываем в массив
 
-            if (rectangles.Length == 0)
+            if (rectangles.Length == 0) // если нечего рисовать
                 return;
 
-            if (isFilled)
-                graphics.FillRectangles(brush, rectangles);
-            else
-                graphics.DrawRectangles(pen, rectangles);
+            if (isFilled)   // если стоит галочка рисовать закрашенные прямоугольники
+                graphics.FillRectangles(brush, rectangles); // рисуем закрашенный прямоуглоьник
+            else            
+                graphics.DrawRectangles(pen, rectangles);   // рисуем обычный прямоуглоьник
         }
 
-        private void ClearAll(object sender, EventArgs e)
+        private void ClearAll(object sender, EventArgs e)  // очистка всего и перерисовка всех фигур
         {
             figures.Clear();
             RedrawContent(true);
         }
 
-        private void Undo(object sender, EventArgs e)
+        private void Undo(object sender, EventArgs e)  // при нажатии кнопки undo
         {
-            if (figures.All(fig => !fig.IsDrawn))
+            if (figures.All(fig => !fig.IsDrawn))   //  проверка если все фигуры уже ненарисованы, то выход
                 return;
+ 
+            figures.Last(fig => fig.IsDrawn).IsDrawn = false;  //иначе берём последнюю нарисованную фигуру и ставим флаг не отрисовывать false
 
-            figures.Last(fig => fig.IsDrawn).IsDrawn = false;
-
-            RedrawContent(true);
+            RedrawContent(true);    // перерисовываем все фигуры
         }
 
-        private void Redo(object sender, EventArgs e)
+        private void Redo(object sender, EventArgs e) // при нажатии кнопки redo
         {
-            if (figures.All(fig => fig.IsDrawn))
+            if (figures.All(fig => fig.IsDrawn))    //  проверка если все фигуры уже нарисованы, то выход
                 return;
 
-            figures.First(fig => !fig.IsDrawn).IsDrawn = true;
-            RedrawContent(true);
+            figures.First(fig => !fig.IsDrawn).IsDrawn = true;  //иначе берём последнюю ненарисованную фигуру и ставим флаг отрисовывать true
+            RedrawContent(true);    // перерисовываем все фигуры
         }
 
-        private void RedoLastThree(object sender, EventArgs e)
+        private void RedoLastThree(object sender, EventArgs e)  // при нажатии кнопки redo3 (по твоему заданию)
         {
-            if (figures.All(fig => fig.IsDrawn))
+            if (figures.All(fig => fig.IsDrawn))    //  проверка если все фигуры уже нарисованы, то выход
                 return;
 
-            var values = figures.Where(fig => !fig.IsDrawn).ToList();
+            var values = figures.Where(fig => !fig.IsDrawn).ToList();   // в values записывются все ненарисованные фигуры
 
-            values.Take(values.Count > 3 ? 3 : values.Count).ToList().ForEach(v => v.IsDrawn = true);
-            RedrawContent(true);
+            values.Take(values.Count > 3 ? 3 : values.Count)    // если длина values > 3 то берём первые 3 фигуры 
+                                                                // (иначе если длина меньше 3ёх, то берём сколько есть)
+            .ToList().ForEach(v => v.IsDrawn = true);           // для каждого элемента ставим флаг отрисовывать true
+                                                                                                        
+            
+            RedrawContent(true);    // перерисовываем все фигуры
         }
 
-        private void OnMouseDown(object sender, MouseEventArgs e)
+        private void OnMouseDown(object sender, MouseEventArgs e)   // как только нажали мышь
         {
-            if (e.Button == MouseButtons.Left)
+            if (e.Button == MouseButtons.Left)  // проверка наажли ли мы левую кнопку мыши
             {
-                startCursorPoint = endCursorPoint = e.Location;
-                isPainting = true;
+                startCursorPoint = endCursorPoint = e.Location; //запоминаем начальную и сразу конечную позицию курсора 
+                                                                //(вдруг пользователь не будет рисовать и нажмёт на мышку случайно)
+                isPainting = true;                              // тот самый флаг, что мы в находимся в процессе рисования ставим true
             }
         }
 
-        private void OnMouseMove(object sender, MouseEventArgs e)
+        private void OnMouseMove(object sender, MouseEventArgs e)   // вызывается как мы водим мышь по канвасу (даже когда не нажали на мышку)
         {
-            if (isPainting && e.Button == MouseButtons.Left)
+            if (isPainting && e.Button == MouseButtons.Left)    // проверям нажата ли левая клавиша мыши 
+                                                                // (т.е. проверяем рисуем ли мы или просто водим мышку по экрану)
             {
-                endCursorPoint = e.Location;
-                canvas.Refresh();
+                endCursorPoint = e.Location;                    // запоминаем позицию курсора
+                canvas.Refresh();                               // обновляем сам canvas необходимо, чтобы прямоугольник постоянно обновлял свои размеры, когда мы двигаем мышку
             }
         }
 
-        private void OnMouseUp(object sender, MouseEventArgs e)
+        private void OnMouseUp(object sender, MouseEventArgs e)     // когда отпустили клавишу мыши
         {
-            figures.Add(new MyRectangle(rectangle));
+            figures.Add(new MyRectangle(rectangle));                // доабвляем новую фигуру
             isPainting = false;
         }
 
